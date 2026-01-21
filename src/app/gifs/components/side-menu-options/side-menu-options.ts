1885fac5033275a11038a8e9562dd1c280156ef3
Germán Balaguera <german.balaguera@oudos-it.com>
2026-01-20 20:22:21 -0500

    Refactorización de  las opciones del menú lateral para utilizar enrutamiento dinámico y actualizar estilos

--- src/app/gifs/components/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  sublaberl: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions { }
+export class SideMenuOptions {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      sublaberl: 'Gifs Populares'
+    },
+        {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      route: '/dashboard/search',
+      sublaberl: 'Buscar Gifs'
+    }
+  ];
+}
