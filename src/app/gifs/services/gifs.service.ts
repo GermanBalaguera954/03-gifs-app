288186b6e05021a596684a28c38d89f182624c5a
Germán Balaguera <germanbalaguera954@gmail.com>
2026-01-26 19:13:26 -0500

    Se agrego interfaz Gif y GifMapper para el manejo de respuestas de Giphy; actualizar GifService para usar señales

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,22 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interface';
+import { GifMapper } from '../mapper/gif.mapper';
+import { Gif } from '../interfaces/gif.interface';
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
   private http = inject(HttpClient);
+
+  trendingGifs = signal<Gif[]>([]);
 
   constructor() {
     this.loadTrendingGifs();
   }
 
   loadTrendingGifs() {
-    this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
-      params: {
-        api_key: environment.apiKey,
-        limit: 25,
-      },
-    });
+    this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
+        params: {
+          api_key: environment.apiKey,
+          limit: 25,
+        },
+      })
+      .subscribe((response) => {
+        const gifs = GifMapper.mapGiphyResponseToGifs(response.data);
+        this.trendingGifs.set(gifs);
+        console.log(gifs);
+      });
   }
 }
